797bc16ae619e5ec9e85061d4a7a64c729e5a92a
sustained <sustained.dissonance@gmail.com>
2019-08-22 09:01:53 +0100

    Add pages/:project (view individual project).

---
+++ pages/projects/_project.vue
@@ -0,0 +1,25 @@
+<template>
+  <section>
+    <card :title="project.attributes.title">
+      <DynamicMarkdown
+        v-bind="project"
+        :custom-attributes="{ custom: 'Hello!' }"
+      />
+    </card>
+
+    <card title="Raw Project">
+      <pre>{{ project }}</pre>
+    </card>
+  </section>
+</template>
+
+<script>
+import { getMeta, setHead, asyncData } from 'nuxt-dynamic-markdown/lib/mixin'
+import Card from '~/components/Card'
+
+export default {
+  components: { Card },
+  mixins: [getMeta, setHead],
+  asyncData
+}
+</script>
