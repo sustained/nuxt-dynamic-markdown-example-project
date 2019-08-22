5fb8bf1117620a88d927f1aff3a314a4e2b6d14e
sustained <sustained.dissonance@gmail.com>
2019-08-22 09:01:35 +0100

    Add pages/index (list projects).

---
+++ pages/projects/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <section class="section">
+    <div class="columns is-mobile">
+      <card
+        v-for="(project, index) in projects"
+        :key="index"
+        :title="project.title"
+        icon="alert-decagram"
+      >
+        <nuxt-link
+          :to="{ name: 'projects-project', params: { project: project.slug } }"
+        >
+          {{ project.title }}
+
+          {{ project }}
+        </nuxt-link>
+      </card>
+    </div>
+  </section>
+</template>
+
+<script>
+import { getEntities } from 'nuxt-dynamic-markdown/lib/mixin'
+import Card from '~/components/Card'
+
+export default {
+  components: { Card },
+  asyncData: getEntities('projects')
+}
+</script>
