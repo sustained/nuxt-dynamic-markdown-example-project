e13a4bb22233bffb005aab3993defc3388a6f865
sustained <sustained.dissonance@gmail.com>
2019-08-26 10:13:50 +0100

    Add locale switcher.

--- pages/index.vue
+++ pages/index.vue
@@ -2,33 +2,30 @@
   <section class="section">
     <div class="columns is-mobile">
       <card title="Free" icon="github-circle">
-        Open source on
-        <a href="https://github.com/buefy/buefy">
-          GitHub
-        </a>
+        <nuxt-link
+          v-for="locale in availableLocales"
+          :key="locale.code"
+          :to="switchLocalePath(locale.code)"
+          class="button mr-4"
+          >{{ locale.name }}</nuxt-link
+        >
       </card>
 
       <card title="Responsive" icon="cellphone-link">
-        <b class="has-text-grey">
-          Every
-        </b>
+        <b class="has-text-grey">Every</b>
         component is responsive
       </card>
 
       <card title="Modern" icon="alert-decagram">
         Built with
-        <a href="https://vuejs.org/">
-          Vue.js
-        </a>
+        <a href="https://vuejs.org/">Vue.js</a>
         and
-        <a href="http://bulma.io/">
-          Bulma
-        </a>
+        <a href="http://bulma.io/">Bulma</a>
       </card>
 
-      <card title="Lightweight" icon="arrange-bring-to-front">
-        No other internal dependency
-      </card>
+      <card title="Lightweight" icon="arrange-bring-to-front"
+        >No other internal dependency</card
+      >
     </div>
   </section>
 </template>
@@ -41,6 +38,15 @@ export default {
 
   components: {
     Card
+  },
+
+  computed: {
+    availableLocales() {
+      // WORKAROUND: https://github.com/nuxt-community/nuxt-i18n/issues/205
+      return this.$root.$i18n.locales.filter(
+        (lang) => lang.code !== this.$i18n.locale
+      )
+    }
   }
 }
 </script>
